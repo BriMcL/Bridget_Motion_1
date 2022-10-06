1dacc732c228f1eddda9ab8c64e03cdad67457c1
BriMcL <102632152+BriMcL@users.noreply.github.com>
2022-10-06 11:09:27 -0400

    buttonmedia

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -17,8 +17,31 @@ var mainTL=gsap.timeline();
 mainTL.add(heroAnimation());
 
 
+<<<<<<< Updated upstream
 var buttonAnimation = gsap.timeline({paused:true});
 buttonAnimation.to("#trail-btn",{duration:0.25, scale:2})
+=======
+let mm = gsap.matchMedia();
+
+var buttonSize = 1;
+
+
+mm.add("(min-width: 800px)", () => {
+  // desktop setup code here...
+  buttonSize = 2;
+});
+
+mm.add("(max-width: 799px)", () => {
+  // mobile setup code here...
+  buttonSize = 1.25;
+});
+
+
+
+
+var buttonAnimation = gsap.timeline({paused:true});
+buttonAnimation.to("#trail-btn",{duration:0.25, scale:buttonSize})
+>>>>>>> Stashed changes
     .to("#trail-btn i",{duration:0.25, rotateY: 180})
     .to("#first-line",{duration:0.25, alpha:0, y:50})
     .to("#second-line",{duration:0.25, alpha:0, y:25});
