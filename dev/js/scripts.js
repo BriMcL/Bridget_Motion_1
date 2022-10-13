ed84b47182260882304032acaac5eeabc4c1ac03
BriMcL <102632152+BriMcL@users.noreply.github.com>
2022-10-13 19:03:08 -0400

    sfbjasbf

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -58,8 +58,9 @@ button.addEventListener("mouseout",function(){
 
 function quoteAnimation(){
   var tl =gsap.timeline({scrollTrigger:{trigger:"#hero-2", scrub:true, end:"top 40%", start:"top 80%"}});
-  tl.from("#bg-img",{duration:2.5, clipPath:"inset(0 50%)", ease:"bounce.out(1,0.3)"}, "startSplit")
+  tl.from("#bg-img",{duration:2.5, clipPath:"inset(0 50%)", ease:"back.out(1,0.3)"}, "startSplit")
   .from("#hero-2 h1",{duration:2, clipPath:"inset(0 50%)"},"-=25%","startSplit")
+  .from("#color",{duration:2, clipPath:"inset(0 50%)"},"-=25%","startSplit")
   .from("#hero-2 h2",{duration:2, clipPath:"inset(0 50%)"},"-=5%");
   return tl;
 }
