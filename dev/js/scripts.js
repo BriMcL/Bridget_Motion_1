f346b34e60e3b08ae0c43cb6a3b7759a7bde54ac
BriMcL <102632152+BriMcL@users.noreply.github.com>
2022-10-13 17:13:17 -0400

    vkbsjkv

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,9 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
+
+// Sets
 gsap.set("button i",{transformOrigin:"center bottom"});
 
 function heroAnimation(){
@@ -14,7 +18,8 @@ function heroAnimation(){
 
 
 var mainTL=gsap.timeline();
-mainTL.add(heroAnimation());
+mainTL.add(heroAnimation())
+      .add(quoteAnimation());
 
 
 let mm = gsap.matchMedia();
@@ -51,7 +56,72 @@ button.addEventListener("mouseout",function(){
   buttonTL.reverse();
 })
 
- 
+function quoteAnimation(){
+  var tl =gsap.timeline({scrollTrigger:{trigger:"#hero-2", scrub:true, end:"top 40%", start:"top 80%"}});
+  tl.from("#bg-img",{duration:5, clipPath:"inset(0 50%)"}, "startSplit")
+  .from("#hero-2 h1",{duration:2, clipPath:"inset(0 50%)"},"-=25%","startSplit")
+  .from("#hero-2 h2",{duration:2, clipPath:"inset(0 50%)"},"-=5%");
+  return tl;
+}
+
+
+
+// gsap.set("#hero h1 span",{alpha:0.25})
+
+// var heroButtonTL = gsap.timeline({paused:true});
+//     heroButtonTL.to("#trails-btn",{duration:0.25,scale:2, backgroundColor:"#881d02"},"trigger")  
+//     .to("#first-line",{duration:0.25, alpha:0, y:50},"trigger")
+//     .to("#second-line",{duration:0.25, alpha:0, y:20},"trigger")
+//     .to("#trails-btn i",{duration:0.25, rotateY:180},"-=0.15");
+
+
+// var trailsBtn = document.querySelector("#trails-btn");
+
+// trailsBtn.addEventListener("mouseover",function(){
+//     heroButtonTL.play();
+// })
+
+// trailsBtn.addEventListener("mouseout",function(){
+//     heroButtonTL.reverse();
+// })
+
+// function heroAnimation(){
+//     var tl = gsap.timeline();
+//     tl.from("#first-line",{duration:1,alpha:0, y:-100})
+//     .from("#second-line",{duration:1,alpha:0, y:-100},"-=0.75")
+//     .from("#trails-btn",{duration:1,y:100, alpha:0},"-=.5")
+//     .from("#trails-btn i",{duration:0.5,rotation:90, alpha:0, transformOrigin: "left bottom"},"-=0.5");
+//     return tl;
+// }
+
+
+// function boxAnimation(){
+//     var tl = gsap.timeline({scrollTrigger: { trigger: "#box", markers: false, scrub:true, end:"bottom center", start:"top 80%"}});
+    
+//     tl.from("#box", {duration: 1,alpha: 0,rotation: -180,x: "-=300%"})
+//       .to("#box", {duration: 1 ,rotation: 48,y: 200})
+//       .to("#box", {duration: 1 ,y: -600, scale:2});
+//     return tl;
+// }
+
+
+// function peakAnimation(){
+//     var tl = gsap.timeline({scrollTrigger:{trigger:"#hiking", scrub:true,markers:false, end:"top 30%"}});
+//     tl.from("#hiking aside div",{duration:1, scale:3, alpha:0},"startPeak")
+//     .from("#hiking h1",{duration:1,x:"-=200%", alpha:0},"startPeak")
+//     .from("#hiking p",{duration:1,x:"-=200%", alpha:0},"startPeak")
+//     return tl;
+// }
+
+
+
+
+
+// var mainTimeline = gsap.timeline();
+// mainTimeline.add(heroAnimation())
+//     .add(boxAnimation())
+//     .add(peakAnimation())
+
  
 
 
